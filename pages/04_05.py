c9d861c8dd049f5cf47e7a69232170367c18ca78
yitai77 <135570508+yitai77@users.noreply.github.com>
2023-06-27 15:24:33 +0900

    05

--- pages/04_05.py
+++ pages/04_05.py
@@ -7,11 +7,21 @@ import seaborn as sns
 
 Korean = common.get_ko_data()
 
-rating_counts = Korean['rating'].value_counts().head(10)
+ko_rating_counts = Korean['rating'].value_counts().head(10)
 
 fig, ax = plt.subplots(figsize=(10,6))
-ax.pie(rating_counts, labels=rating_counts.index,autopct='%1.1f%%', colors=['skyblue', 'limegreen', 'orange', 'red', 'purple'])
+ax.pie(ko_rating_counts, labels=ko_rating_counts.index,autopct='%1.1f%%', colors=['skyblue', 'limegreen', 'orange', 'red', 'purple'])
 
 ax.set_title('Distribution of Top 10 Ratings for Netflix Content in South Korea')
 
 st.pyplot(fig)
+
+
+US =common.get_us_data()
+
+us_rating_counts = US['rating'].value_counts().head(10)
+
+fig, ax = plt.subplots(figsize=(10,6))
+ax.pie(us_rating_counts, labels=us_rating_counts.index,autopct='%1.1f%%', colors=['skyblue', 'limegreen', 'orange', 'red', 'purple'])
+
+ax.set_title('Distribution of Top 10 Ratings for Netflix Content in US')
