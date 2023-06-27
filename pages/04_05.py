72e550426b8a586a64b2aa5d66794ff19673d7f1
yitai77 <135570508+yitai77@users.noreply.github.com>
2023-06-27 15:25:59 +0900

    05

--- pages/04_05.py
+++ pages/04_05.py
@@ -9,19 +9,21 @@ Korean = common.get_ko_data()
 
 ko_rating_counts = Korean['rating'].value_counts().head(10)
 
-fig, ax = plt.subplots(figsize=(10,6))
+fig1, ax = plt.subplots(figsize=(10,6))
 ax.pie(ko_rating_counts, labels=ko_rating_counts.index,autopct='%1.1f%%', colors=['skyblue', 'limegreen', 'orange', 'red', 'purple'])
 
 ax.set_title('Distribution of Top 10 Ratings for Netflix Content in South Korea')
 
-st.pyplot(fig)
+st.pyplot(fig1)
 
 
 US =common.get_us_data()
 
 us_rating_counts = US['rating'].value_counts().head(10)
 
-fig, ax = plt.subplots(figsize=(10,6))
-ax.pie(us_rating_counts, labels=us_rating_counts.index,autopct='%1.1f%%', colors=['skyblue', 'limegreen', 'orange', 'red', 'purple'])
+fig2, ax1 = plt.subplots(figsize=(10,6))
+ax1.pie(us_rating_counts, labels=us_rating_counts.index,autopct='%1.1f%%', colors=['skyblue', 'limegreen', 'orange', 'red', 'purple'])
 
-ax.set_title('Distribution of Top 10 Ratings for Netflix Content in US')
+ax1.set_title('Distribution of Top 10 Ratings for Netflix Content in US')
+
+st.pyplot(fig2)
