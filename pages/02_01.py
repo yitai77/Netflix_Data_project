7e3ef6ac143cec3d0397fe073c98de28e48e54d6
yitai77 <135570508+yitai77@users.noreply.github.com>
2023-06-27 14:20:47 +0900

    01

--- pages/02_01.py
+++ pages/02_01.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import common
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,4 +18,4 @@ plt.pie(sk_data_counts, labels=sk_data_counts.index, autopct='%1.1f%%', startang
 plt.axis('equal')
 plt.title('Netflix Shows in the South Korea')
 
-st.write(plt)
+st.pyplot()
