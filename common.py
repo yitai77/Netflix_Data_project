2453bfdf8191e4ce95c06d9d14be9124b435e9ca
yitai77 <135570508+yitai77@users.noreply.github.com>
2023-06-27 12:40:47 +0900

    data

--- common.py
+++ common.py
@@ -2,14 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+data = pd.read_csv("./netflix1.csv")
 @st.cache_data
 def get_us_data():
-
-    data = pd.read_csv("./netflix1.csv")
 
     usa_data = data[data['country'] == 'United States']
     return usa_data
 
+def get_ko_data():
+    sk_data = data[data['country'] == 'South Korea']
+    return sk_data
+
+    
 def page_config():
     st.set_page_config(
         page_title="Netflix : South Korea and United States",
